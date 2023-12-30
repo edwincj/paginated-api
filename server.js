dc63bc6d0da13f63a47dc0ba6b6f584a8e069f5b
edwincj <edwincj1998@gmail.com>
2023-12-30 23:06:18 +0530

    used a middleware to use pagination for any given models

--- server.js
+++ server.js
@@ -21,26 +21,52 @@ const users = [
     {name:"User 17", id: 17}
 ]
 
-app.get('/users', (req, res) => {
-    const {page, limit} = req.query;
-    const pageNum = parseInt(page);
-    const limitNum =parseInt(limit)
-    const start = (pageNum - 1) * limitNum;
-    const end = pageNum * limitNum;
-    const data = users.slice(start, end);
-    const result = {data};
-    if (end < users.length)
-        result.next = {
-            limit: limitNum,
-            page: pageNum + 1,
-        };
-    if (start > 0)
-        result.previous = {
-            limit: limitNum,
-            page: pageNum - 1,
-        };
+const posts = [
+    {name:"Post 1", id: 1},
+    {name:"Post 2", id: 2},
+    {name:"Post 3", id: 3},
+    {name:"Post 4", id: 4},
+    {name:"Post 5", id: 5},
+    {name:"Post 6", id: 6},
+    {name:"Post 7", id: 7},
+    {name:"Post 8", id: 8},
+    {name:"Post 9", id: 9},
+    {name:"Post 10", id: 10},
+    {name:"Post 11", id: 11},
+    {name:"Post 12", id: 12},
+    {name:"Post 13", id: 13}
+]
 
-    res.json(result);
-})
+const paginatedResults = model => {
+    return (req, res, next) => {
+        const {page, limit} = req.query;
+        const pageNum = parseInt(page);
+        const limitNum =parseInt(limit)
+        const start = (pageNum - 1) * limitNum;
+        const end = pageNum * limitNum;
+        const data = model.slice(start, end);
+        const result = {data};
+        if (end < model.length)
+            result.next = {
+                limit: limitNum,
+                page: pageNum + 1,
+            };
+        if (start > 0)
+            result.previous = {
+                limit: limitNum,
+                page: pageNum - 1,
+            };
+        res.paginatedResult = result;
+        next();
+    }
+}
+
+app.get('/users', paginatedResults (users), (req, res) => {
+    res.json(res.paginatedResult);
+});
+
+app.get('/posts', paginatedResults (posts), (req, res) => {
+    res.json(res.paginatedResult);
+});
 
 app.listen(3000);
